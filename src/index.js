b954dd2b5789bb0bf07dca51b7136299acd7aa1a
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-28 23:14:07 -0500

    Added text and changed size of chart.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import * as d3 from 'd3'
 
 const dataURL = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 
@@ -6,7 +5,7 @@ const promiseData = async () => {
     const res = await fetch(dataURL)
     const json = await res.json()
     console.log(json)
-    return json.data
+    return json
 }
 
 const month = ['January', 'February', 'March','April', 'May','June','July', 'August','September','October','November','December']
@@ -18,43 +17,56 @@ const formatTooltip  = data => {
     return `\$${data[1]} Billion<br/><span class='date'>${date}</span>`
 }
 
-const loadViz = data => {
+const loadViz = json => {
 
-    const barWidth = 4
+    const data = json.data
+
+    const barWidth = 3
 
     const scaled = d3.scaleLinear()
         .domain([0, d3.max(data, d => d[1])])
-        .range([0, 580])
+        .range([0, 480])
 
-    const div = d3.select('body').append('div')    
-        .attr('class', 'tooltip')                
+    const div = d3.select('body').append('div')
+        .attr('class', 'tooltip')
             .style('opacity', 0)
 
     const svg = d3.select('body')
         .append('svg')
         .attr('width', barWidth * data.length + 20)
-        .attr('height', 600)
+        .attr('height', 500)
+
+    svg.append('text')
+        .attr('x', 30)
+        .attr('y', 50)
+        .text(json.source_name)
+
+    svg.append('text')
+        .attr('x', 32)
+        .attr('y', 72)
+        .attr('class', 'gdp')
+        .text(`Quarterly ${json.name.split(',')[0]} (billions)`)
 
     const bar = svg.selectAll('g')
         .data(data)
           .enter().append('g')
-          .attr('transform', (d, i) => `translate(${i * barWidth + 10}, ${595 - scaled(d[1])})`)
+          .attr('transform', (d, i) => `translate(${i * barWidth + 10}, ${490 - scaled(d[1])})`)
 
     bar.append('rect')
         .attr('width', barWidth - 1)
         .attr('height', d => scaled(d[1]))
         .attr('fill', 'black')
-        .on('mouseover', d => {        
-            div.transition()        
-                .duration(200)        
-                .style('opacity', .9)        
-            div.html(formatTooltip(d))    
-                .style('left', (d3.event.pageX) + 'px')        
-                .style('top', (d3.event.pageY - 28) + 'px');    
-        }).on('mouseout', function(d) {        
-            div.transition()        
-                .duration(500)        
-                .style('opacity', 0);    
+        .on('mouseover', d => {
+            div.transition()
+                .duration(200)
+                .style('opacity', .9)
+            div.html(formatTooltip(d))
+                .style('left', (d3.event.pageX) + 'px')
+                .style('top', (d3.event.pageY - 72) + 'px');
+        }).on('mouseout', function(d) {
+            div.transition()
+                .duration(500)
+                .style('opacity', 0);
         })
 
 }
